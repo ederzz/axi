1251dcbba1465f89b68c14d3f53b79215d049fb9
shenyiling <jiangzx@getui.com>
2020-07-10 17:28:31 +0800

    chore: add terser.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,30 @@
 import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
+import { terser } from "rollup-plugin-terser";
 
 export default {
     input: 'src/index.ts',
     output: [
-        { file: pkg.main, format: 'umd', name: 'Axi' },
-        { file: pkg.module, format: 'es' }
+        { 
+            file: pkg.main, 
+            format: 'umd', 
+            name: 'Axi',  
+            banner: '/* Axi version: ' + pkg.version + ' */',
+            footer: '/* Contribute to axi: https://github.com/shenyiling/axi */'
+        },
+        { 
+            file: pkg.module, 
+            format: 'es',
+            banner: '/* Axi version: ' + pkg.version + ' */',
+            footer: '/* Contribute to axi: https://github.com/shenyiling/axi */'
+        }
     ],
-    plugins: [typescript()]
+    plugins: [
+        typescript(),
+        terser({
+            output: {
+                comments: 'all'
+            }
+        })
+    ],
 }
