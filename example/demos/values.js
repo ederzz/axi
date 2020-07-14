230a3a239821bff6c141f6145e1e094300a8e5a0
shenyiling <jzx19951028yyjy@163.com>
2020-07-14 20:40:35 +0800

    add new demo

--- example/demos/values.js
+++ example/demos/values.js
@@ -9,6 +9,7 @@ addDemos(
             title: 'FUNCTION BASED VALUES', 
             count: 3,
             shape: 'circle',
+            size: 'small',
             axiParams: [
                 {
                     target: '.function-based-values .sprite',
@@ -28,6 +29,88 @@ addDemos(
                     direction: 'alternate',
                     loop: true
                 }
+            ]
+        },
+        { 
+            id: 'relative', 
+            cls: 'relative', 
+            title: 'FUNCTION BASED VALUES', 
+            shape: 'circle',
+            axiParams: [
+                {
+                    target: '.relative .sprite',
+                    translateX: {
+                        value: 250,
+                        duration: 1000
+                    },
+                    width: {
+                        value: 8,
+                        duration: 1800,
+                        easing: 'easeInOutSine'
+                    },
+                    rotate: {
+                        value: '2turn',
+                        duration: 1800,
+                        easing: 'easeInOutSine'
+                    },
+                    direction: 'alternate'
+                }
+            ]
+        },
+        { 
+            id: 'colors', 
+            cls: 'colors', 
+            title: 'COLORS', 
+            count: 5,
+            shape: 'circle',
+            size: 'small',
+            spriteCls: [ 'color-hex', 'color-rgb', 'color-hsl', 'color-rgba', 'color-hsla' ],
+            axiParams: [
+                {
+                    target: '.color-hex',  
+                    translateX: 270,
+                    background: '#FFF',
+                    easing: 'easeInOutQuad',
+                    loop: true,
+                    direction: 'alternate',
+                    duration: 2000,
+                },
+                {
+                    target: '.color-rgb',  
+                    translateX: 270,
+                    duration: 2000,
+                    easing: 'easeInOutQuad',
+                    loop: true,
+                    direction: 'alternate',
+                    background: 'rgb(255,255,255)'
+                },
+                {
+                    target: '.color-hsl',  
+                    duration: 2000,
+                    easing: 'easeInOutQuad',
+                    loop: true,
+                    translateX: 270,
+                    direction: 'alternate',
+                    background: 'hsl(0, 100%, 100%)'
+                },
+                {
+                    duration: 2000,
+                    easing: 'easeInOutQuad',
+                    loop: true,
+                    target: '.color-rgba',  
+                    translateX: 270,
+                    direction: 'alternate',
+                    background: 'rgba(255,255,255, .2)'
+                },
+                {
+                    duration: 2000,
+                    easing: 'easeInOutQuad',
+                    loop: true,
+                    target: '.color-hsla',  
+                    translateX: 270,
+                    direction: 'alternate',
+                    background: 'hsla(0, 100%, 100%, .2)'
+                },
             ]
         },
     ]
